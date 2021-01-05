ab5abab251938450cd4594e7ba4186e51796d158
Marcelloni Esteban Eduardo <emarcelloni@agdcorp.com.ar>
2021-01-04 23:21:43 -0300

    estoy creando el brodcasting entre los sockets

--- src/service/p2p.js
+++ src/service/p2p.js
@@ -2,6 +2,7 @@ import WebSocket from 'ws'
 
 const {P2P_PORT = 5000, PEERS} = process.env; //PEERS es una variable de entorno que tiene un String de nodos (URL) por defecto separdos por coma donde poder conectarnos.
 const peers = PEERS ? PEERS.split(',') : []; //Si PEERS tiene datos, devuelvo un Array con los peers (URL) o sino un array vacio.
+const MESSAGE = {BLOCKS:'blocks'} //Creo una constante para tipificar los distintos tipos de mensajes que vamos a ir pasando
 
 class P2PService{
 	constructor(blockchain){
@@ -11,19 +12,29 @@ class P2PService{
 
 	listen(){
 		const server = new WebSocket.Server({ port: P2P_PORT }); //Crea un Server WebSocket
-		server.on('connection',(socket)=>{this.onConnection(socket)});
+		server.on('connection',(socket)=>{this.onConnection(socket)}); //Cuando un socket se conecta a este server lo agrega a la lista this.sockets[]
 
 		peers.forEach((peer)=>{ //Me conecto a una lista de peers y me agrego a la misma
 			const socket = new WebSocket(peer); //Esto crea un Socket Cliente
-			socket.on('open',()=>{this.onConnection(socket)})
+			socket.on('open',()=>{this.onConnection(socket)}) //Cuando me conecto a un server de PEERS agegro esa conexion a this.sockets[]
 		});
 
 		console.log(`Service ws: ${P2P_PORT} listening...`);
 	}
 
+	//Cuando alguien se conecta a mi o yo me conecto a alguien se ejecuta este metodo.
 	onConnection(socket){
+		const {'blockchain':{blocks}} = this;
 		console.log('[ws:socket] connected.');
 		this.sockets.push(socket); //Agrego el nuevo Socket al array Sockets
+		//Para saber si recibo un mensaje me suscribo a la accion "message"
+		socket.on('message',(message)=>{
+			const{type,value} = JSON.parse(message); //Con parse, paso el texto recibido a Json.
+		});
+		//Cuando me conecto o se conectan a mi les voy a enviar la lista de bloques que tenemos.
+		socket.send(JSON.stringify({type:MESSAGE.BLOCKS,value:blocks})); //Con stringify paso el Json a texto
+		
+		console.log({type, value});
 	}
 }
 
