a0adb5a8cb87345456b92006db0c0c9bdcf64e61
Marcelloni Esteban Eduardo <emarcelloni@agdcorp.com.ar>
2021-01-28 17:02:32 -0300

    Clase Miner, se codifico puntos 1, 2 y 3.

--- src/miner/miner.js
+++ src/miner/miner.js
@@ -1,3 +1,5 @@
+import { Transaction, blockchainWallet }  from '../wallet'
+
 class Miner{
 
 	constructor(blockchain, p2pService, wallet){
@@ -7,16 +9,28 @@ class Miner{
 	};
 
 	mine() {
-		const { blockchain: { memoryPool } } = this;
+		const { 
+			blockchain: { memoryPool },
+			p2pService, 
+			wallet } = this;
+		if(memoryPool.transactions.length === 0) throw Error('There are no unconfirmed transactions.');
+
 		/*
-		1. Include reward to miner in transaction.
-		2. Create a block consisting on valid transactions.
-		3. Sync new blockchain with the network.
 		4. Wipe transactions from memory pool.
 		5. Broadcasting wipe message to every node.
-		6. 
 		*/
-	};
+		
+		// 1. Include reward to miner in transaction.
+		memoryPool.transactions.push(Transaction.reward(wallet, blockchainWallet));
+		
+		// 2. Create a block consisting on valid transactions.
+		const block = this.blockchain.addBlock(memoryPool.transactions);
+		
+		// 3. Sync new blockchain with the network.
+		p2pService.syn();
+
+		return block;
+	}
 
 }
 
